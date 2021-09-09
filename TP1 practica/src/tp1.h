96fd89a00e4088fddc93433ad821638f7c599f2f
Keppler515 <maxisosa25@hotmail.com>
2021-09-08 23:20:01 -0300

    Practica de primer TP. Falta la factorización

---
+++ TP1 practica/src/tp1.h
@@ -0,0 +1,18 @@
+/*
+ * tp1.h
+ *
+ *  Created on: 8 sep. 2021
+ *      Author: maxis
+ */
+
+#ifndef TP1_H_
+#define TP1_H_
+
+
+int sumar(int a, int b);
+int restar(int a, int b);
+int multiplicar(int a, int b);
+float dividir(int a, int b, float* pDir);
+
+
+#endif /* TP1_H_ */
